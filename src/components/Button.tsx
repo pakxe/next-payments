c4a5e7960a7b08bd55805bc6eb0b09343bed4b38
pakxe <pigkill40@naver.com>
2025-02-06 21:43:16 +0900

    feat: button 컴포넌트 구현

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,5 @@
 import {WithChildren} from '@/types/with';
-import {css} from '@emotion/react';
+import {css, useTheme} from '@emotion/react';
 import {ComponentProps} from 'react';
 
 type Props = ComponentProps<'button'> &
@@ -8,9 +8,21 @@ type Props = ComponentProps<'button'> &
   };
 
 const Button = ({fullWidth, children, ...attrs}: Props) => {
+  const {colors} = useTheme();
+
   return (
     <button
       css={css`
+        background-color: ${colors.gray5};
+        color: ${colors.gray1};
+        padding: 12px 0;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        border-radius: 8px;
+
         width: ${fullWidth ? '100%' : 'auto'};
       `}
       {...attrs}
