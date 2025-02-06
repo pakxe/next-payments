1f713480fc63b54b9dcff6797f3b71b29683be48
pakxe <pigkill40@naver.com>
2025-02-06 21:47:32 +0900

    feat: 스켈레톤 자동 생성기 구현

---
+++ src/components/SkeletonBlinker.tsx
@@ -0,0 +1,64 @@
+import {SerializedStyles, useTheme} from '@emotion/react';
+import {Children, cloneElement, isValidElement, memo, ReactElement, ReactNode} from 'react';
+import Skeleton from './Skeleton';
+
+const getCSS = props => {
+  return props.cssProp ? props.cssProp : props.css ? props.css : {};
+};
+
+const SkeletonUI = ({cssProp}: {cssProp?: SerializedStyles}) => {
+  const {skeletonCSS} = useTheme();
+
+  return <span css={[skeletonCSS, cssProp]} />;
+};
+
+const executeAndTransform = (Component: any, props: any, isLoading: boolean): ReactNode => {
+  const jsx = Component({...props, isLoading});
+  return jsx;
+};
+
+const transformSkeleton = (node: ReactElement | ReactElement[], isLoading: boolean): ReactNode => {
+  if (Array.isArray(node)) {
+    return node.map(child => transformSkeleton(child, isLoading));
+  }
+
+  if (!isValidElement(node)) {
+    return node; // 문자열, 숫자 등 기본 노드는 그대로 반환
+  }
+
+  const {type, props} = node;
+
+  // 1️⃣ `data-skeleton`이 있는 요소라면 무조건 스켈레톤 UI로 변경
+  if (props['data-skeleton']) {
+    if (type.name === 'Text' && isLoading) return <Skeleton.Text type={props.type} cssProp={props.cssProp} />;
+    if ('alt' in props && isLoading) return <Skeleton.Image {...props} />;
+
+    return isLoading ? <SkeletonUI cssProp={getCSS(props)} /> : node;
+  }
+
+  // 기본 html
+  if (typeof type === 'string') {
+    return cloneElement(node, {
+      children: Children.map(props.children, child => transformSkeleton(child, isLoading)),
+    });
+  }
+
+  // 사용자 정의 컴포넌트 까기 전
+  if (typeof type === 'function') {
+    const jsx = executeAndTransform(type, props, isLoading);
+    return transformSkeleton(jsx, isLoading);
+  }
+
+  // 까진거 | emotion element
+  return cloneElement(node, {
+    children: Children.map(props.children, child => transformSkeleton(child, isLoading)),
+  });
+};
+
+const SkeletonBlinker = memo(({children, isLoading}: {children: ReactNode; isLoading: boolean}) => {
+  const transformedChildren = transformSkeleton(children, isLoading);
+
+  return <>{transformedChildren}</>;
+});
+
+export default SkeletonBlinker;
