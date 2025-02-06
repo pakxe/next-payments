4332f646f8cb74847da828cdcbd51a76390eeb2d
pakxe <pigkill40@naver.com>
2025-02-06 21:42:21 +0900

    design: 스크롤 css 제거

--- src/GlobalStyle.ts
+++ src/GlobalStyle.ts
@@ -132,4 +132,10 @@ export const GlobalStyle = css`
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
+
+  scrollbar-width: none; /* Firefox */
+  -ms-overflow-style: none; /* IE and Edge */
+  &::-webkit-scrollbar {
+    display: none; /* Chrome, Safari, Opera */
+  }
 `;
