a1abc8e1a96730c2b5b4701660aab96f57462896
pakxe <pigkill40@naver.com>
2025-02-06 21:49:01 +0900

    feat: Layout을 _app에 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Layout from '@/components/Layout';
 import {GlobalStyle} from '@/GlobalStyle';
 import theme from '@/theme';
 import {Global, ThemeProvider} from '@emotion/react';
@@ -7,7 +8,9 @@ export default function App({Component, pageProps}: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Global styles={GlobalStyle} />
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ThemeProvider>
   );
 }
