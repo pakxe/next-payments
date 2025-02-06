970b7d723c2c6c400533eab7f741033821644c33
pakxe <pigkill40@naver.com>
2025-02-06 23:55:57 +0900

    chore: 배포 세팅

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  basePath: process.env.NODE_ENV === 'production' ? '/next-payments' : '',
+  output: 'export',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/next-payments' : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
